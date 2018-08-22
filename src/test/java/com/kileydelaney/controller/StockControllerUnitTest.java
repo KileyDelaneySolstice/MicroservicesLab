54cffb4fa0983ad4820ab454207f4607a9dcf566
Kiley Delaney <kdelaney@solstice.com>
2018-08-22 15:11:24 -0500

    Completes controller integration tests

--- src/test/java/com/kileydelaney/controller/StockControllerUnitTest.java
+++ src/test/java/com/kileydelaney/controller/StockControllerUnitTest.java
@@ -4,7 +4,6 @@ import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.*;
 
-import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Matchers.any;
 import static org.mockito.Mockito.when;
 
@@ -17,6 +16,9 @@ import org.mockito.InjectMocks;
 import org.mockito.Mockito;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.sql.Date;
+import java.sql.Timestamp;
 
 
 @RunWith(SpringRunner.class)
@@ -38,22 +40,35 @@ public class StockControllerUnitTest{
     @Test
     public void testAddStock() throws Exception {
         Stock mockStock = new Stock();
+        mockStock.setId(101L);
         mockStock.setSymbol(2);
-        mockStock.setVolume(200);
         mockStock.setPrice(19.99);
+        mockStock.setVolume(200);
+        Timestamp testDate = new Timestamp(2018,8,22,2,29,56,33);
+        Date testDateOnly = new Date(2018,8,22);
+        mockStock.setDate(testDate);
+        mockStock.setDateOnly(testDateOnly);
+
 
         when(stockController.add(any(Stock.class)))
                 .thenReturn(mockStock);
 
         Stock aStock = new Stock();
+        aStock.setId(102L);
         aStock.setSymbol(5);
         aStock.setPrice(20.99);
+        aStock.setVolume(100);
+        aStock.setDate(new Timestamp(2018,8,31,6,22,21,33));
+        aStock.setDateOnly(new Date(2018,8,31));
 
         Stock retStock = stockController.add(aStock);
 
+        assertThat(retStock.getId(), is(equalTo(101L)));
         assertThat(retStock.getSymbol(), is(equalTo(2)));
-        assertThat(retStock.getVolume(), is(equalTo(200)));
         assertThat(retStock.getPrice(), is(equalTo(19.99)));
+        assertThat(retStock.getVolume(), is(equalTo(200)));
+        assertThat(retStock.getDate(), is(equalTo(testDate)));
+        assertThat(retStock.getDateOnly(), is(equalTo(testDateOnly)));
 
     }
 
