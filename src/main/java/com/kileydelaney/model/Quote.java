b667ccfe41c07839c6a0cb8ff6c294bfe95eec7f
Kiley Delaney <kdelaney@solstice.com>
2018-08-23 11:09:46 -0500

    Fills in quote service (with successful JSON parsing)

--- src/main/java/com/kileydelaney/model/Quote.java
+++ src/main/java/com/kileydelaney/model/Quote.java
@@ -3,8 +3,17 @@ package com.kileydelaney.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.sun.org.apache.xpath.internal.operations.Quo;
+import sun.java2d.pipe.SpanShapeRenderer;
 
+import java.net.URL;
 import java.sql.Timestamp;
+import java.text.SimpleDateFormat;
+import java.util.Arrays;
+import java.util.List;
+import static java.util.Arrays.asList;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class Quote {
@@ -22,6 +31,57 @@ public class Quote {
     private Timestamp date;
 
 
+    public void Quote() {}
 
+    public void Quote(int symbolId, int volume, double price, Timestamp date) {
+        this.symbolId = symbolId;
+        this.volume = volume;
+        this.price = price;
+        this.date = date;
+    }
+
+
+    public String getSymbolName() {
+        Symbol symbol = new Symbol(this.symbolId);
+        return symbol.getName();
+    }
+
+    public int getSymbolId() {
+        return symbolId;
+    }
+
+    public int getVolume() {
+        return volume;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public Timestamp getDate() {
+        return date;
+    }
+
+    @Override
+    public String toString() {
+        return "Stock [symbol=" + getSymbolName() + ", volume=" + volume + ", price=$" + price + ", date=" + date.toString();
+    }
+
+
+    /**
+     * Helper method to parse JSON data from URL
+     */
+    public static List<Quote> jsonToList(String url) throws Exception {
+
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.enable(SerializationFeature.INDENT_OUTPUT);
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy HH:mm:ss.SSSZ");
+
+        mapper.setDateFormat(dateFormat);
+
+        Quote[] quoteList = mapper.readValue(new URL(url), Quote[].class);
+
+        return Arrays.asList(quoteList);
+    }
 
 }
