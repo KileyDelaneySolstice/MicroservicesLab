e357db59b1714017f88692c8b2dce7d83415db80
Kiley Delaney <kdelaney@solstice.com>
2018-08-27 15:38:23 -0500

    Most recent version before pcf push

---
+++ src/main/java/com/kileydelaney/MicroservicesLabApplication.java
@@ -0,0 +1,16 @@
+package com.kileydelaney;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class MicroservicesLabApplication {
+    
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroservicesLabApplication.class, args);
+    }
+
+
+}
